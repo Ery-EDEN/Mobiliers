d13894f2102f4dd3bc328ec8ef32a44dff31562f
Ery-EDEN <ery-github@gmail.com>
2021-02-01 16:01:47 +0100

    First commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+// Database  params
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'eval_annonce');
+
+define('APP_ROOT', dirname(dirname(__FILE__)));
+
+define('URL_ROOT', 'http://localhost/eval-annonce');
+
+define('SITE_NAME', "Mon site d'annonce");
